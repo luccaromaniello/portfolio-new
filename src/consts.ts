65b801b9be100322b06344f7e1efa53e178a61d2
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-09-09 15:44:45 -0300

    Initialize project

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Lucca Romaniello';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+// styles
+export const TOP_NAV_HEIGHT = "56px";
