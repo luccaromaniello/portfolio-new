aec8f5470bd6a6d1a3099cfe7bb31ce544c8c610
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-09-11 22:49:50 -0300

    feat: add external link to topnav

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,7 @@
 export const SITE_TITLE = 'Lucca Romaniello';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 
-// styles
-export const TOP_NAV_HEIGHT = "56px";
+export const GITHUB_URL = "https://github.com/luccaromaniello/";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
+export const MAIL_URL = "mailto:contact@luccaromaniello.com";
+export const WEBSITE_URL = "https://www.luccaromaniello.com";
